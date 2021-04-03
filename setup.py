658b980fff5edcc3feebb432f9a8a2bc4adf4f13
Lars Eppinger <lars.eppinger@posteo.de>
2021-04-03 23:14:20 +0200

    Corrected name, bumped version

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 
 setup(
-    name="EasyConfig",
-    packages=["EasyConfig"],
-    version="0.1.0",
+    name="easyconfig",
+    packages=["easyconfig"],
+    version="0.1.1",
     license="MIT",
     description="An easy-to-use configuration package using YAML",
     author="Lars Eppinger",
     author_email="pypi@eppinger.dev",
     url="https://github.com/lars250698/easyconfig",
-    download_url="https://github.com/lars250698/easyconfig/archive/v_0_1_0.tar.gz",
+    download_url="https://github.com/lars250698/easyconfig/archive/v_0_1_1.tar.gz",
     keywords=["config", "configuration", "yaml", "easyconfig"],
     requires=["pyyaml"],
     classifiers=[
